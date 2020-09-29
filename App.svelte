30b9be835154a85d49efca0c713b9693b7b351e8
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-29 10:41:08 +0700

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Kanan from "./Kanan.svelte";
+</script>
+
+<style>
+  :global(body) {
+    background: rgb(6, 26, 26);
+    color: #fffddb;
+    font-family: "Cousine", monospace;
+  }
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  p{
+display: flex;
+flex: 1 3 100px;
+justify-content: center;
+  }
+</style>
+
+<main>
+	<h2>Hello Everyone</h2>
+	<p>My Name is Nisa, I Love the Logic Thinks about User-Interface and User-Experience</p>
+	<Kanan />
+	<Kanan />
+	<Kanan />
+</main>
---
+++ Kanan.svelte
@@ -0,0 +1,54 @@
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+:root {
+  --pink: #FF7591;
+  --ungu: #BE9EDF;
+}
+html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto Mono', monospace;
+}
+p::selection, a::selection, h1::selection {
+    background: var(--ungu);
+}
+nav{
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    background: var(--pink);    
+}
+nav > img{
+    width: auto;
+    height: 180px;
+}
+.main{
+    background: var(--ungu); 
+}
+.kanan{
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
+
+<nav>
+        <div class="kanan">
+            <h1>Hi, Simpleazy!</h1>
+        <p>Aku adalah Asisten Bendahara Sekolah mu, yang siap bantuin kamu di Segala bentuk pencatatan atau pembungkuan keuangan kelas.. &#128176;</p>
+        </div>        
+        <img src="https://res.cloudinary.com/codelifings/image/upload/v1596531968/ice-cream_tv7wto.png">
+        </nav>
+				<nav class="main">
+          <div class="kanan">
+              <h1>Hi, Simpleazy!</h1>
+          <p>Aku adalah Asisten Bendahara Sekolah mu, yang siap bantuin kamu di Segala bentuk pencatatan atau pembungkuan keuangan kelas.. &#128176;</p>
+          </div>        
+          <img src="https://res.cloudinary.com/codelifings/image/upload/v1596531968/ice-cream_tv7wto.png">
+        </nav>
